260ecd0c1c723245bdbc82a35ccd721be355d411
Mario Boseski <crboseskimario@gmail.com>
2026-01-27 14:55:04 +0100

    style: improve layout in Auth.jsx

--- src/pages/Auth.jsx
+++ src/pages/Auth.jsx
@@ -10,9 +10,11 @@ const Auth = () => {
   }
 
   return (
-    <div>
-      {isLogin ? <LoginForm /> : <RegisterForm />}
-      <button onClick={toggleAuthMode}>{isLogin ? "Don't have an account? Register" : "Already have an account? LogIn"}</button>
+    <div className="container">
+      <div className="flex flex-col justify-center items-center min-h-dvh gap-2 p-2">
+        {isLogin ? <LoginForm /> : <RegisterForm />}
+        <button onClick={toggleAuthMode}>{isLogin ? "Don't have an account? Register" : "Already have an account? LogIn"}</button>
+      </div>
     </div>
   );
 }
