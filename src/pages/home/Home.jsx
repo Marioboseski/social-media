ca1123059f235740e07eb73ce913bb0319fc3b4f
Mario Boseski <crboseskimario@gmail.com>
2026-01-28 14:57:17 +0100

    Add styles and improve layout in Home.jsx and Post.jsx

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,15 +3,18 @@ import postsData from "../../data/postsData";
 
 const Home = () => {
   return (
-    <div>
-      {postsData.map((post) => (
-        <Post
-          key={post.id}
-          avatar={post.avatar}
-          username={post.username}
-          post={post.post}
-          caption={post.caption} />
-      ))}
+    <div className="container">
+      <div className="flex flex-col justify-center items-center gap-2">
+        {postsData.map((post) => (
+          <Post
+            key={post.id}
+            avatar={post.avatar}
+            username={post.username}
+            post={post.post}
+            caption={post.caption} 
+            icons={post.icons}/>
+        ))}
+      </div>
     </div>
   );
 }
--- src/pages/home/Post.jsx
+++ src/pages/home/Post.jsx
@@ -1,10 +1,20 @@
-const Post = ({ avatar, username, post, caption }) => {
+const Post = ({ avatar, username, post, caption, icons }) => {
   return (
-    <div>
-      <p>{username}</p>
-      <img src={avatar} alt="avatar" className="w-full max-w-6 rounded-full"/>
+    <div className="flex flex-col p-2 gap-2">
+      <div className="flex gap-3 items-center ">
+        <img src={avatar} alt="avatar" className="w-full max-w-6 rounded-full" />
+        <p>{username}</p>
+      </div>
       <img src={post} alt="post" className="w-full max-w-[450px]" />
-      <p>{caption}</p>
+      <div className="flex gap-3">
+        {icons.map((Icon, index) => (
+          <Icon key={index} />
+        ))}
+      </div>
+      <div className="flex gap-3">
+        <p>{username}</p>
+        <p>{caption}</p>
+      </div>
     </div>
   );
 }
