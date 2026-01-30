2ea1b05a0d452536e00b07518599096747076652
Mario Boseski <crboseskimario@gmail.com>
2026-01-30 13:30:32 +0100

    Feat: add bookmark toggle to post component, Post.jsx

--- src/pages/home/Post.jsx
+++ src/pages/home/Post.jsx
@@ -1,23 +1,39 @@
-const Post = ({ avatar, username, post, caption, icons, VerifIcon }) => {
+import { Bookmark } from "lucide-react";
+import { useBookmarks } from "../../context/BookmarksContext";
+
+const Post = ({ post }) => {
+  const { bookmarks, toggleBookmark } = useBookmarks();
+
+  const isBookmarked = bookmarks.some(item => item.id === post.id);
+
   return (
     <div className="flex flex-col p-2 gap-2">
-      <div className="flex gap-3 items-center ">
-        <img src={avatar} alt="avatar" className="w-full max-w-6 rounded-full" />
-        <p>{username}</p>
+      
+      <div className="flex gap-3 items-center">
+        <img src={post.avatar} alt="avatar" className="w-10 h-10 rounded-full" />
+        <p>{post.username}</p>
+        <post.VerifIcon />
       </div>
-      <img src={post} alt="post" className="w-full max-w-[450px]" />
-      <div className="flex gap-3">
-        {icons.map((Icon, index) => (
+
+      <img src={post.post} alt="post" className="w-full max-w-[450px]" />
+
+      <div className="flex gap-3 items-center">
+        {post.icons.map((Icon, index) => (
           <Icon key={index} />
         ))}
+
+        <button onClick={() => toggleBookmark(post)}>
+          <Bookmark fill={isBookmarked ? "black" : "none"} />
+        </button>
       </div>
+
       <div className="flex gap-1">
-        <p>{username}</p>
-        <VerifIcon/>
-        <p>{caption}</p>
-      </div>      
+        <p className="font-semibold">{post.username}</p>
+        <p>{post.caption}</p>
+      </div>
+
     </div>
   );
-}
+};
 
 export default Post;
