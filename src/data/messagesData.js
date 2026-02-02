5bffc6b9092eab35f6a029704cf8109427edf5fd
Mario Boseski <crboseskimario@gmail.com>
2026-02-02 13:34:41 +0100

    Add new data in messagesData.js

--- src/data/messagesData.js
+++ src/data/messagesData.js
@@ -1,5 +1,8 @@
 import avatar1 from "../assets/posts/avatar1.jpg";
 import avatar2 from "../assets/posts/avatar2.jpg";
+import avatar3 from "../assets/posts/avatar3.jpg";
+import avatar4 from "../assets/posts/avatar4.jpg";
+import avatar5 from "../assets/posts/avatar5.jpg";
 
 const messagesData = [
   {
@@ -14,6 +17,33 @@ const messagesData = [
   {
     id: 2,
     avatar: avatar2,
+    username: "max_ovcharenko",
+    lastMessage: "Hey how are you",
+    time: "24 minutes ago",
+    isOnline: false,
+  },
+
+  {
+    id: 3,
+    avatar: avatar3,
+    username: "max_ovcharenko",
+    lastMessage: "Hey how are you",
+    time: "24 minutes ago",
+    isOnline: false,
+  },
+
+  {
+    id: 4,
+    avatar: avatar4,
+    username: "max_ovcharenko",
+    lastMessage: "Hey how are you",
+    time: "24 minutes ago",
+    isOnline: false,
+  },
+
+  {
+    id: 5,
+    avatar: avatar5,
     username: "max_ovcharenko",
     lastMessage: "Hey how are you",
     time: "24 minutes ago",
