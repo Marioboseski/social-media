8ee8df4aa2dececc42ac6215dc0db0ddd8517683
Mario Boseski <crboseskimario@gmail.com>
2026-02-08 19:16:06 +0100

    Improve navbar layout

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,15 +1,14 @@
 import { Link } from "react-router-dom";
-import { House } from "lucide-react";
-import { CircleArrowOutUpRightIcon } from "lucide-react";
-import { Send } from "lucide-react";
-import { Bookmark } from "lucide-react";
-import { User } from "lucide-react";
+import { House, CircleArrowOutUpRightIcon, Send, Bookmark, User } from "lucide-react";
 
 const Navbar = () => {
   return (
     <div className="flex justify-center items-center">
-      <nav className="fixed bottom-0 w-full max-w-[450px] bg-white p-3">
-        <ul className="flex justify-around">
+      <nav className="fixed bottom-0 w-full max-w-[450px] bg-white p-3 md:top-0 md:left-0
+        md:w-full md:max-w-32
+         md:border-r-2
+        md:flex md:justify-center">
+        <ul className="flex justify-around items-center md:flex-col md:justify-evenly">
           <Link to={"/home"}><House size={33}/></Link>
           <Link to={"/bookmarks"}><Bookmark size={33}/></Link>
           <Link to={"/explore"}><CircleArrowOutUpRightIcon size={33}/></Link>
