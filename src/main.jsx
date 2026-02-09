837880ceb96494ac1a44605be4113353f98611ea
Mario Boseski <crboseskimario@gmail.com>
2026-02-09 17:06:30 +0100

    Fix: use HashRouter for GitHub Pages routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import BookmarksProvider from "./context/BookmarksContext.jsx";
 import UserProvider from "./context/ProfileContext.jsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <BookmarksProvider>
         <UserProvider>
           <App />
         </UserProvider>
       </BookmarksProvider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
